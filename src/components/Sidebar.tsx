c19da0f28e06df5ecc3a27dee929c7a243586e4d
cvssdae <cvssdae@gmail.com>
2026-08-04 15:53:26 +0900

    사이드바 그린으로 수정

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -60,7 +60,7 @@ export default function Sidebar() {
 
   return (
     <>
-      <header className="bg-red-600 text-white sticky top-0 z-50 shadow-md border-b border-red
+      <header className="bg-green-600 text-white sticky top-0 z-50 shadow-md border-b border-green
       -700 font-sans">
         <div className="max-w-7xl mx-auto px-4 md:px-6 flex items-center justify-between h-14">
           
