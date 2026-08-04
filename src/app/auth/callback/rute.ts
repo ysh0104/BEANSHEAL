a92342dedc250dae6633c9ff2df8016fe7d3cd87
ysh0104 <ysh0104@users.noreply.github.com>
2026-08-04 14:22:25 +0900

    add auth callback route

--- src/app/auth/callback/rute.ts
+++ src/app/auth/callback/rute.ts
@@ -1,4 +1,4 @@
-import { supabase } from "@/utils/supabase";
+import { createClient } from "@supabase/supabase-js";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
@@ -6,6 +6,10 @@ export async function GET(request: Request) {
   const code = searchParams.get("code");
 
   if (code) {
+    const supabase = createClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    );
     await supabase.auth.exchangeCodeForSession(code);
   }
 
